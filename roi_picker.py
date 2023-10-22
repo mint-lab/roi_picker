1aa9071be126546c48d3f67f6c62a540334eb82a
Sunglok Choi <sunglok@hanmail.net>
2023-10-22 14:56:53 +0900

    Remove lib.py and roi_tool.py

--- roi_picker.py
+++ roi_picker.py
@@ -1,6 +1,56 @@
-from lib import *
+import numpy as np
+import cv2 as cv
+import json
+import math
+import argparse
+import json
+import os
+import opencx as cx
+from random import randrange
 
-# ===========================================================================================
+
+RADIUS = 5
+FINAL_LINE_COLOR = (255, 255, 255)
+WORKING_LINE_COLOR = (127, 127, 127)
+FONT = cv.FONT_HERSHEY_DUPLEX
+FONTSCALE = 1 # FONTSCALE
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (255, 0, 0)
+THICKNESS = 1 # Line thickness of 1 px
+POLY_X_AXIS = 30
+LINE_X_AXIS = 380
+PTS_X_AXIS = 690
+Y_AXIS = 20
+
+KEY_CTRL_M = ord('M') - ord('A') + 1 # CTRL+M: Toggle polygon mode while removing all points
+KEY_CTRL_N = ord('N') - ord('A') + 1 # CTRL+N: Remove all points for the current ROI
+KEY_CTRL_A = ord('A') - ord('A') + 1 # CTRL+A: Add a new ROI
+KEY_CTRL_D = ord('D') - ord('A') + 1 # CTRL+D: Delete the current ROI
+KEY_CTRL_X = ord('X') - ord('A') + 1 # CTRL+S: Save(append) ROIs to `roi_file_name.json`
+KEY_CTRL_Q = ord('Q') - ord('A') + 1 # CTRL+Q: Add new point
+KEY_CTRL_W = ord('W') - ord('A') + 1 # CTRL+W: Add new line
+KEY_CTRL_E = ord('E') - ord('A') + 1 # CTRL+Q: Scale up 
+KEY_CTRL_R = ord('R') - ord('A') + 1 # CTRL+R: Scale down 
+KEY_CTRL_L = ord('L') - ord('A') + 1 # CTRL+L: Load ROI
+ESC = 27                             # Escape ROI
+
+POLYGON = 'Polygon'
+LINE = 'Line'
+POINT = 'Point'
+WINDOWNAME = "ROI_PICKER" # Name for our window
+
+POLY_IND = 1
+POINT_IND = 2
+LINE_IND = 3
+
+ROI_WEIGHT = 0.4
+THRESHOLD = 0.5
+ZOOM_LEVEL = 10
+ZOOM_BOX_RADIUS = 10 
+ZOOM_BOX_MARGIN = 10
+SCALE_DEFAULT = 1
+SCALE_STEP = 0.05
 
 class PolygonDrawer(object):
     def __init__(self, img_load_path, result_folder):
@@ -324,12 +374,20 @@ class PolygonDrawer(object):
         rand_color = (r, g, b)
         return rand_color
 
-# ============================================================================
 
-# if __name__ == "__main__":
 
-#     img_path_load = 'image/210812_camera400.png'
-#     save_folder = 'results'
+if __name__ == "__main__":
+    # Add arguments to the parser
+    parser = argparse.ArgumentParser(description='ROI')
+    parser.add_argument('--org', type=str, help='Image path to load')
+    parser.add_argument('--des', type=str, help='Image path to save')
 
-#     pd = PolygonDrawer(img_path_load, save_folder)
-#     image = pd.roi_process()
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Access the values of the arguments
+    img_path_load = args.org
+    img_path_save = args.des
+
+    pd = PolygonDrawer(img_path_load, img_path_save)
+    image = pd.roi_process()
